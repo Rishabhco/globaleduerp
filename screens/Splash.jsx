5cb4f76140470449a4cccf120ee62401202425ac
Rishabhco <rishabhagrawal2020@gmail.com>
2023-07-27 12:37:07 +0530

    changes in splash screen

--- screens/Splash.jsx
+++ screens/Splash.jsx
@@ -1,19 +1,9 @@
 import React,{useEffect} from 'react';
-import { PermissionsAndroid,View, Image, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Splash = ({navigation}) => {
-    const checkApplicationPermission = async () => {
-        if (Platform.OS === 'android') {
-            try {
-                await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-    };
     useEffect(() => {  
-        checkApplicationPermission();
         AsyncStorage.getItem('isLoggedIn').then(val=>{
             if(val==="true"){
                 setTimeout(() => {
